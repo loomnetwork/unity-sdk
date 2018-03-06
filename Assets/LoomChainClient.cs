aa857cbaa1576ad762138df217f06f9635a3602c
Vadim Macagon <vadim.macagon@gmail.com>
2018-03-06 21:41:05 +0700

    Add Tx encoding & signing
    Commiting the tx doesn't quite work yet, getting back an error:
    "Timed out waiting for transaction to be included in a block"

---
+++ Assets/LoomChainClient.cs
@@ -0,0 +1,133 @@
+﻿using Google.Protobuf;
+using Chaos.NaCl;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.Networking;
+using System;
+using Newtonsoft.Json;
+
+internal class TxJsonRpcRequest
+{
+    [JsonProperty("jsonrpc")]
+    public string Version { get; set; }
+
+    [JsonProperty("method")]
+    public string Method { get; set; }
+
+    [JsonProperty("params")]
+    public string[] Params { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    public TxJsonRpcRequest(string method, string[] args, string id = "")
+    {
+        Version = "2.0";
+        Method = method;
+        Params = args;
+        Id = id;
+    }
+}
+
+public class BroadcastTxResult
+{
+    [JsonProperty("code")]
+    public string Code { get; set; }
+
+    [JsonProperty("data")]
+    public string Data { get; set; }
+
+    [JsonProperty("log")]
+    public string Log { get; set; }
+
+    [JsonProperty("hash")]
+    public string Hash { get; set; }
+}
+
+public class BroadcastTxResponse
+{
+    [JsonProperty("jsonrpc")]
+    public string Version { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("result")]
+    public BroadcastTxResult Result { get; set; }
+}
+
+public class LoomChainClient
+{
+    private string url;
+
+    public LoomChainClient(string url)
+    {
+        this.url = url;
+    }
+
+    public string SignTx(DummyTx tx, byte[] privateKey, byte[] publicKey)
+    {
+        var txBytes = tx.ToByteArray();
+        var sigBytes = Ed25519.Sign(txBytes, privateKey);
+
+        var signer = new Signer
+        {
+            Signature = ByteString.CopyFrom(sigBytes),
+            PublicKey = ByteString.CopyFrom(publicKey)
+        };
+
+        var signedTx = new SignedTx
+        {
+            Inner = tx.ToByteString(),
+            Signers = { signer }
+        };
+
+        var payload = CryptoBytes.ToBase64String(signedTx.ToByteArray());
+        Debug.Log("Signed Tx: " + payload);
+        return payload;
+    }
+
+    public async Task<BroadcastTxResult> CommitTx(string signedTx)
+    {
+        var req = new TxJsonRpcRequest("broadcast_tx_commit", new string[] { signedTx }, "whatever");
+        return (await this.PostTx(req)).Result;
+    }
+
+    private async Task<BroadcastTxResponse> PostTx(TxJsonRpcRequest tx)
+    {
+        string body = JsonConvert.SerializeObject(tx);
+        Debug.Log("PostTx Body: " + body);
+        byte[] bodyRaw = new UTF8Encoding().GetBytes(body);
+        using (var r = new UnityWebRequest(this.url, "POST"))
+        {
+            r.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            r.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            r.SetRequestHeader("Content-Type", "application/json");
+            await r.SendWebRequest();
+            HandleError(r);
+            if (r.downloadHandler != null && !String.IsNullOrEmpty(r.downloadHandler.text))
+            {
+                Debug.Log("Response: " + r.downloadHandler.text);
+                return JsonConvert.DeserializeObject<BroadcastTxResponse>(r.downloadHandler.text);
+            }
+        }
+        return null;
+    }
+
+    private static void HandleError(UnityWebRequest r)
+    {
+        if (r.isNetworkError)
+        {
+            throw new System.Exception(String.Format("HTTP '{0}' request to '{1}' failed", r.method, r.url));
+        }
+        else if (r.isHttpError)
+        {
+            if (r.downloadHandler != null && !String.IsNullOrEmpty(r.downloadHandler.text))
+            {
+                // TOOD: extract error message if any
+            }
+            throw new System.Exception(String.Format("HTTP Error {0}", r.responseCode));
+        }
+    }
+}
--- Assets/authSample.cs
+++ Assets/authSample.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using Chaos.NaCl;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class authSample : MonoBehaviour {
     public Text statusTextRef;
+
+    private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
 
     // Use this for initialization
     void Start () {
@@ -19,5 +24,24 @@ public class authSample : MonoBehaviour {
         var authClient = new LoomAuthClient();
         var account = await authClient.SignInFromNativeApp();
         this.statusTextRef.text = "Signed in as " + account.Username;
+    }
+
+    public async void SendTx()
+    {
+        var chainClient = new LoomChainClient("http://stage-rancher.loomapps.io:46657");
+        var tx = new DummyTx
+        {
+            Val = "Hello World!"
+        };
+        // TODO: use LoomAccount.PrivateKey
+        byte[] randomBytes = new byte[32];
+        rngCsp.GetBytes(randomBytes);
+        var privateKeySeed = randomBytes;
+        byte[] publicKey;
+        byte[] privateKey;
+        Ed25519.KeyPairFromSeed(out publicKey, out privateKey, privateKeySeed);
+
+        var payload = chainClient.SignTx(tx, privateKey, publicKey);
+        var result = await chainClient.CommitTx(payload);
     }
 }
