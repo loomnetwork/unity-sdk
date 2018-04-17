80cfc6ca70ee41e2b16998f834efe7bf1260dd35
Vadim Macagon <vadim.macagon@gmail.com>
2018-04-17 21:47:44 +0700

    (WIP) Upgrade Unity SDK to work with the latest Loom SDK

--- Assets/authSample.cs
+++ Assets/authSample.cs
@@ -53,7 +53,7 @@ public class authSample : MonoBehaviour
         this.statusTextRef.text = "Signed in as " + this.identity.Username;
 
         // This DAppChain client will connect to the example REST server in the Loom Go SDK. 
-        this.chainClient = new DAppChainClient("http://localhost:8998")
+        this.chainClient = new DAppChainClient("http://localhost", 8998, 9999)
         {
             TxMiddleware = new TxMiddleware(new ITxMiddlewareHandler[]{
                 new SignedTxMiddleware(this.identity.PrivateKey)
@@ -80,11 +80,25 @@ public class authSample : MonoBehaviour
         this.statusTextRef.text = "Committed Tx to Block " + result.Height;
     }
 
+    // NOTE: The structure of the query params is defined by the contract author,
+    // the only constraint is that it must be serializable to JSON.
+    private class QueryParams
+    {
+        public string Body { get; set; }
+    }
+
+    // NOTE: The structure of the query result is defined by the contract author,
+    // the only constraint is that it must be deserializable from JSON.
+    private class QueryResult
+    {
+        public string Body { get; set; }
+    }
+
     public async void Query()
     {
+        var contract = "0x0";
         // NOTE: Query results can be of any type that can be deserialized via Newtonsoft.Json.
-        var lastKey = await this.chainClient.QueryAsync<string>("app/last-key");
-        var data = await this.chainClient.QueryAsync<DummyTx>("app/dummy/" + lastKey);
-        this.statusTextRef.text = string.Format("Last Key: {0} & Value: {1}", data.Key, data.Val);
+        var result = await this.chainClient.QueryAsync<QueryResult>(contract, new QueryParams{ Body = "hello" });
+        this.statusTextRef.text = "Query Response: " + result.Body;
     }
 }
