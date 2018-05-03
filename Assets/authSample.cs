2c10166a0cd47090deddcbf4923431ab99700af2
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-03 15:10:29 +0700

    Add a button to the sample scene to reset the private key

--- Assets/authSample.cs
+++ Assets/authSample.cs
@@ -2,11 +2,15 @@
 using UnityEngine.UI;
 using Loom.Unity3d;
 using Newtonsoft.Json;
+using Google.Protobuf;
+using System;
+using System.Threading.Tasks;
 
 public class authSample : MonoBehaviour
 {
     public Text statusTextRef;
 
+    private Address contractAddr;
     private Identity identity;
     private Address callerAddr;
     private DAppChainClient chainClient;
@@ -25,13 +29,13 @@ public class authSample : MonoBehaviour
 
     }
 
-    public async void SignIn()
+    private IAuthClient CreateAuthClient()
     {
 #if !UNITY_WEBGL
         try
         {
             CertValidationBypass.Enable();
-            var authClient = AuthClientFactory.Configure()
+            return AuthClientFactory.Configure()
                 .WithLogger(Debug.unityLogger)
                 .WithClientId("25pDQvX4O5j7wgwT052Sh3UzXVR9X6Ud") // unity3d sdk
                 .WithDomain("loomx.auth0.com")
@@ -40,13 +44,40 @@ public class authSample : MonoBehaviour
                 .WithScope("openid profile email picture")
                 .WithRedirectUrl("http://127.0.0.1:9999/auth/auth0/")
                 .Create();
+        }
+        finally
+        {
+            CertValidationBypass.Disable();
+        }
+#else
+        return AuthClientFactory.Configure()
+            .WithLogger(Debug.unityLogger)
+            .WithPrivateKeyLocalStoragePath("loomUserInfo")
+            .Create();
+#endif
+    }
+
+#if !UNITY_WEBGL // In WebGL all interactions with the key store should be done in the host page.
+    private async Task<IKeyStore> CreateKeyStore(string accessToken)
+    {
+        return await KeyStoreFactory.CreateVaultStore(new VaultStoreConfig
+        {
+            Url = "https://stage-vault.delegatecall.com/v1/",
+            VaultPrefix = "unity3d-sdk",
+            AccessToken = accessToken
+        });
+    }
+#endif
+
+    public async void SignIn()
+    {
+#if !UNITY_WEBGL
+        try
+        {
+            CertValidationBypass.Enable();
+            var authClient = this.CreateAuthClient();
             var accessToken = await authClient.GetAccessTokenAsync();
-            var keyStore = await KeyStoreFactory.CreateVaultStore(new VaultStoreConfig
-            {
-                Url = "https://stage-vault.delegatecall.com/v1/",
-                VaultPrefix = "unity3d-sdk",
-                AccessToken = accessToken
-            });
+            var keyStore = await this.CreateKeyStore(accessToken);
             this.identity = await authClient.GetIdentityAsync(accessToken, keyStore);
         }
         finally
@@ -54,15 +85,10 @@ public class authSample : MonoBehaviour
             CertValidationBypass.Disable();
         }
 #else
-        var authClient = AuthClientFactory.Configure()
-            .WithLogger(Debug.unityLogger)
-            .WithPrivateKeyLocalStoragePath("loomUserInfo")
-            .Create();
+        var authClient = this.CreateAuthClient();
         this.identity = await authClient.GetIdentityAsync("", null);
-
 #endif
         this.statusTextRef.text = "Signed in as " + this.identity.Username;
-
         // This DAppChain client will connect to the example REST server in the Loom Go SDK. 
         this.chainClient = new DAppChainClient("http://localhost:46657", "http://localhost:47000")
         {
@@ -75,9 +101,36 @@ public class authSample : MonoBehaviour
             },
             new SignedTxMiddleware(this.identity.PrivateKey)
         });
+
+        // There is only one contract address at the moment...
+        this.contractAddr = new Address
+        {
+            ChainId = "default",
+            Local = ByteString.CopyFrom(CryptoUtils.HexStringToBytes("0x005B17864f3adbF53b1384F2E6f2120c6652F779"))
+        };
         this.callerAddr = this.identity.ToAddress("default");
     }
 
+    public async void ResetPrivateKey()
+    {
+#if !UNITY_WEBGL
+        try
+        {
+            CertValidationBypass.Enable();
+            var authClient = this.CreateAuthClient();
+            var accessToken = await authClient.GetAccessTokenAsync();
+            var keyStore = await this.CreateKeyStore(accessToken);
+            this.identity = await authClient.CreateIdentityAsync(accessToken, keyStore);
+        }
+        finally
+        {
+            CertValidationBypass.Disable();
+        }
+#else
+        // TODO
+        throw new NotImplementedException();
+#endif
+    }
     public async void SendTx()
     {
         if (this.identity == null)
@@ -85,7 +138,7 @@ public class authSample : MonoBehaviour
             throw new System.Exception("Not signed in!");
         }
 
-        float r = (Random.value * 100000);
+        float r = (UnityEngine.Random.value * 100000);
         var tx = new DummyTx
         {
             Key = r.ToString(),
@@ -94,7 +147,7 @@ public class authSample : MonoBehaviour
         var contract = new Address
         {
             ChainId = "helloworld",
-            Local = Google.Protobuf.ByteString.CopyFrom(new byte[20])
+            Local = ByteString.CopyFrom(new byte[20])
         };
         var result = await this.chainClient.CallAsync(this.callerAddr, contract, "whatever", tx);
         this.statusTextRef.text = "Committed Tx to Block " + result.Height;
@@ -102,11 +155,9 @@ public class authSample : MonoBehaviour
 
     public async void Query()
     {
-        // TODO: Get the contract address from somewhere.
-        var contract = new Address();
         // TODO: Specify a valid method.
         var method = "mycontract.DoSomething";
-        var result = await this.chainClient.QueryAsync<SampleQueryResult>(contract, method, new SampleQueryParams{ Body = "hello" });
+        var result = await this.chainClient.QueryAsync<SampleQueryResult>(this.contractAddr, method, new SampleQueryParams{ Body = "hello" });
         this.statusTextRef.text = "Query Response: " + result.Body;
     }
 }
