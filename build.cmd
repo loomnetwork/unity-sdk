7e4a3b090baaa7888683c632daf4206a982e912d
Dmitry Khodarenok <dkhodarenok@gmail.com>
2018-06-01 14:46:43 +0700

    updated package name
    package name changed to loom-unity3d-sdk.unitypackage

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-%UNITY_PATH% -projectPath "%~dp0" -batchmode -quit -exportPackage "Assets"  unity3d-identity.unitypackage
+%UNITY_PATH% -projectPath "%~dp0" -batchmode -quit -exportPackage "Assets"  loom-unity3d-sdk.unitypackage
 
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #export UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
-$UNITY_PATH -projectPath "$PWD" -batchmode -quit -exportPackage "Assets"  unity3d-identity.unitypackage
+$UNITY_PATH -projectPath "$PWD" -batchmode -quit -exportPackage "Assets"  loom-unity3d-sdk.unitypackage
