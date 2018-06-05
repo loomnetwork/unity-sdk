18a0a66839207427e15920e5d557698f45d630a5
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-05 17:35:07 +0700

    Remove unused auth related files from WebGL template

--- Assets/WebGLTemplates/Loom/settings.js
+++ Assets/WebGLTemplates/Loom/settings.js
@@ -1,17 +1,3 @@
 window.LOOM_SETTINGS = {
-  transferAsset: null,
-  userInfoStorageKey: 'loomUserInfo',
-  auth: {
-    domain: 'loomx.auth0.com',
-    audience: 'https://keystore.loomx.io/',
-    clientId: '25pDQvX4O5j7wgwT052Sh3UzXVR9X6Ud',
-    // Where Auth0 should redirect after user signs in
-    redirectUrl: window.location.origin,
-    // Where Auth0 should redirect after user signs out
-    logoutUrl: window.location.origin
-  },
-  vault: {
-    url: 'https://stage-vault2.delegatecall.com/v1/',
-    prefix: 'unity3d-sdk'
-  }
+  transferAsset: null
 }
