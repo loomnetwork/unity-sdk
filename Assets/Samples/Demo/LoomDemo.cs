01618b9484e472fa36b6af72247e66d0832802c4
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-05 21:31:33 +0700

    Rename sample scene and move it to Assets/Samples/Demo

--- Assets/Samples/Demo/LoomDemo.cs
+++ Assets/Samples/Demo/LoomDemo.cs
@@ -6,7 +6,7 @@ using System;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-public class authSample : MonoBehaviour
+public class LoomDemo : MonoBehaviour
 {
     public Text statusTextRef;
     public GameObject cube;
