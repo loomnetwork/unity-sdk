dbd7ffc8868ff6876e118fa8bd9c7b353ae676de
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-06-15 14:25:33 +0200

    Added event DTO decoding to EvmChainEventArgs

--- Assets/LoomSDK/EvmChainEventArgs.cs
+++ Assets/LoomSDK/EvmChainEventArgs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Loom.Nethereum.ABI.FunctionEncoding;
 
 namespace Loom.Unity3d {
     public class EvmChainEventArgs : RawChainEventArgs
@@ -10,6 +10,24 @@ namespace Loom.Unity3d {
             : base(contractAddress, callerAddress, blockHeight, data) {
             Topics = topics;
             EventName = eventName;
+        }
+
+        /// <summary>
+        /// Decodes event data into event DTO.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>Decoded event DTO.</returns>
+        /// <see href="https://nethereum.readthedocs.io/en/latest/contracts/calling-transactions-events/"/>
+        public T DecodeEvent<T>() where T : new()
+        {
+            EventTopicDecoder eventTopicDecoder = new EventTopicDecoder();
+            object[] topicStrings = new object[Topics.Length];
+            for (int i = 0; i < Topics.Length; i++)
+            {
+                topicStrings[i] = CryptoUtils.BytesToHexString(Topics[i]);
+            }
+
+            return eventTopicDecoder.DecodeTopics<T>(topicStrings, CryptoUtils.BytesToHexString(Data));
         }
     }
 }
