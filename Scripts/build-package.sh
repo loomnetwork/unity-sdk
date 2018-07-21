b7a0d5dedb1b0c9b43cfba83d4cc08151a34c8de
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-07-21 17:32:48 +0200

    Fixed build-package.sh for macOS

--- Scripts/build-package.sh
+++ Scripts/build-package.sh
@@ -5,7 +5,7 @@ if [ -z "${UNITY_PATH}" ]; then
 fi
 
 DEFAULT_UNITY_PATH=/Applications/Unity/Unity.app/Contents/MacOS/Unity
-if [ -d "${DEFAULT_UNITY_PATH}" ]; then
+if [ -f "${DEFAULT_UNITY_PATH}" ]; then
     echo "Using default Unity path ${DEFAULT_UNITY_PATH}"
     UNITY_PATH=${DEFAULT_UNITY_PATH}
 fi
@@ -15,7 +15,7 @@ if [ -z "${UNITY_PATH}" ]; then
     exit 1
 fi
 
-if [ ! -d "${UNITY_PATH}" ]; then
+if [ ! -f "${UNITY_PATH}" ]; then
     echo "Error: File ${UNITY_PATH} doesn't exist"
     exit /B 1
 fi
