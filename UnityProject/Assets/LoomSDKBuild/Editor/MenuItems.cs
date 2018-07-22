0bdb0c2be7040c1671d448394003ffcc64e28aaa
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-07-22 23:16:31 +0200

    Added script to download samples from GitHub before building the package

---
+++ UnityProject/Assets/LoomSDKBuild/Editor/MenuItems.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+namespace Loom.Client.Unity.Editor.Build {
+    public static class MenuItems {
+        public const string kAssetNameShort = "Loom SDK";
+        public const string kMenuRoot = "Tools/[Build] " + kAssetNameShort + "/";
+
+        [MenuItem(MenuItems.kMenuRoot + "Build Package")]
+        public static void BuildPackage() {
+            PackageBuilder.BuildPackage();
+        }
+
+        [MenuItem(MenuItems.kMenuRoot + "Download Samples .zip's")]
+        public static void DownloadSamples() {
+            SamplesDownloader.DownloadSamples();
+        }
+    }
+}
